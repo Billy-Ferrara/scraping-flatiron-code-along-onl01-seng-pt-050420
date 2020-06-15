e2ab81c67e6aec3fbe74828398e9b1296d164007
Billy Ferrara <willfira717@gmail.com>
2020-06-15 14:47:02 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,14 +10,7 @@ class Scraper
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
 
-  def get_courses
-  end
 
-  def make_courses
-  end
-
-  def print_courses
-  end 
 
 end
 
